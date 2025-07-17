0e638a61c473cacc70c756b85b015c9aa3714ff0
MazingBusiness <mazing.store21@gmail.com>
2025-07-17 12:21:57 +0530

    Hash router setup

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,19 @@ import AppRoutes from "./routes/AppRoutes";
 import "./styles/global.css";
 import "./styles/mainStyle.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    {/* <BrowserRouter basename="/mazing_react_html/"> */}
-    <BrowserRouter basename="/demo_react_mazing_business_second/">
-    {/* <BrowserRouter> */}
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     {/* <BrowserRouter basename="/mazing_react_html/"> */}
+//     <BrowserRouter basename="/demo_react_mazing_business_second/">
+//     {/* <BrowserRouter> */}
+//       <AppRoutes />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>    
       <AppRoutes />
-    </BrowserRouter>
   </React.StrictMode>
 );
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,7 @@
 // src/routes/AppRoutes.jsx
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import Home from "../pages/Home";
 import ProductListing from "../pages/ProductListing";
@@ -15,19 +16,21 @@ import ProfileOrderDetails from "../pages/user-profile/ProfileOrderDetails";
 import PrivateRoute from "./PrivateRoute"; 
 
 const AppRoutes = () => (
-  <Routes>
-    {/* Public Routes */}
-    <Route path="/" element={<Home />} />
-    <Route path="/productListing" element={<ProductListing />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="/forgotpassword" element={<ForgotPassword />} />
-    <Route path="/register" element={<Register />} />
+  <HashRouter>
+    <Routes>
+      {/* Public Routes */}
+      <Route path="/" element={<Home />} />
+      <Route path="/productListing" element={<ProductListing />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/forgotpassword" element={<ForgotPassword />} />
+      <Route path="/register" element={<Register />} />
 
-    {/* Protected Profile Routes */}
-    <Route path="/profileDashbord" element={ <PrivateRoute> <ProfileDashbord /></PrivateRoute> } />
-    <Route path="/profileOrder" element={ <PrivateRoute> <ProfileOrder /> </PrivateRoute> } />
-    <Route path="/profileOrderDetails"element={ <PrivateRoute> <ProfileOrderDetails /></PrivateRoute> } />
-  </Routes>
+      {/* Protected Profile Routes */}
+      <Route path="/profileDashbord" element={ <PrivateRoute> <ProfileDashbord /></PrivateRoute> } />
+      <Route path="/profileOrder" element={ <PrivateRoute> <ProfileOrder /> </PrivateRoute> } />
+      <Route path="/profileOrderDetails"element={ <PrivateRoute> <ProfileOrderDetails /></PrivateRoute> } />
+    </Routes>
+  </HashRouter>
 );
 
 export default AppRoutes;
