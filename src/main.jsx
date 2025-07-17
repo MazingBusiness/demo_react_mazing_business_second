5c2257647f9d84081e26eef402f0e4f1616a564b
MazingBusiness <mazing.store21@gmail.com>
2025-07-17 12:43:17 +0530

    Setup Hash router

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,18 @@ import AppRoutes from "./routes/AppRoutes";
 import "./styles/global.css";
 import "./styles/mainStyle.css";
 
+// For local server
 // ReactDOM.createRoot(document.getElementById("root")).render(
 //   <React.StrictMode>
 //     {/* <BrowserRouter basename="/mazing_react_html/"> */}
-//     <BrowserRouter basename="/demo_react_mazing_business_second/">
-//     {/* <BrowserRouter> */}
+//     <BrowserRouter>
 //       <AppRoutes />
 //     </BrowserRouter>
 //   </React.StrictMode>
 // );
 
+
+// For git upload
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>    
